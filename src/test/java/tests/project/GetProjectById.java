31ef0bc124bd91569e6ef8af389bbb0090543cfe
sivasree-tcm <sivasree@tickingminds.com>
2026-01-21 20:30:16 +0530

    ADDED code for edit and projectbyid

--- src/test/java/tests/project/GetProjectById.java
+++ src/test/java/tests/project/GetProjectById.java
@@ -1,42 +1,42 @@
-//package tests.project;
-//
-//import api.project.ProjectApi;
-//import base.BaseTest;
-//import org.testng.annotations.Test;
-//import tests.user.ApiTestExecutor;
-//import utils.JsonUtils;
-//
-//import java.util.List;
-//
-//public class GetProjectById extends BaseTest {
-//
-//    @Test
-//    public void getProjectByIdApiTest() {
-//
-//        GetProjectByIdTestData testData =
-//                JsonUtils.readJson(
-//                        "testdata/project/getProjectById.json",
-//                        GetProjectByIdTestData.class
-//                );
-//
-//        execute(testData, testData.getTestCases());
-//    }
-//
-//    private void execute(
-//            GetProjectByIdTestData testData,
-//            List<GetProjectByIdTestData.TestCase> cases
-//    ) {
-//
-//        for (GetProjectByIdTestData.TestCase tc : cases) {
-//
-//            ApiTestExecutor.execute(
-//                    testData.getScenario(),
-//                    tc,
-//                    () -> ProjectApi.getProjectById(
-//                            tc.getRequest(),
-//                            tc.getRole()
-//                    )
-//            );
-//        }
-//    }
-//}
+package tests.project;
+
+import api.project.ProjectApi;
+import base.BaseTest;
+import org.testng.annotations.Test;
+import tests.user.ApiTestExecutor;
+import utils.JsonUtils;
+
+import java.util.List;
+
+public class GetProjectById extends BaseTest {
+
+    @Test
+    public void getProjectByIdApiTest() {
+
+        GetProjectByIdTestData testData =
+                JsonUtils.readJson(
+                        "testdata/project/GetProjectById.json",
+                        GetProjectByIdTestData.class
+                );
+
+        execute(testData, testData.getTestCases());
+    }
+
+    private void execute(
+            GetProjectByIdTestData testData,
+            List<GetProjectByIdTestData.TestCase> cases
+    ) {
+
+        for (GetProjectByIdTestData.TestCase tc : cases) {
+
+            ApiTestExecutor.execute(
+                    testData.getScenario(),
+                    tc,
+                    () -> ProjectApi.getProjectById(
+                            tc.getRequest(),
+                            tc.getRole()
+                    )
+            );
+        }
+    }
+}

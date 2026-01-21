02705e1756eadaa12a9b21ff4c81a66c4a6b3176
sivasree-tcm <sivasree@tickingminds.com>
2026-01-21 19:45:25 +0530

    removed code for edit and projectbyid

--- src/test/java/tests/project/EditProjectTest.java
+++ src/test/java/tests/project/EditProjectTest.java
@@ -1,42 +1,42 @@
-package tests.project;
-
-import api.project.ProjectApi;
-import base.BaseTest;
-import org.testng.annotations.Test;
-import tests.user.ApiTestExecutor;
-import utils.JsonUtils;
-
-import java.util.List;
-
-public class EditProjectTest extends BaseTest {
-
-    @Test
-    public void editProjectApiTest() {
-
-        EditProjectTestData testData =
-                JsonUtils.readJson(
-                        "testdata/project/editProject.json",
-                        EditProjectTestData.class
-                );
-
-        execute(testData, testData.getTestCases());
-    }
-
-    private void execute(
-            EditProjectTestData testData,
-            List<EditProjectTestData.TestCase> cases
-    ) {
-
-        for (EditProjectTestData.TestCase tc : cases) {
-
-            ApiTestExecutor.execute(
-                    testData.getScenario(),
-                    tc,
-                    () -> ProjectApi.editProject(
-                            tc.getRequest(),
-                            tc.getRole()
-                    )
-            );
-        }
-    }
-}
+//package tests.project;
+//
+//import api.project.ProjectApi;
+//import base.BaseTest;
+//import org.testng.annotations.Test;
+//import tests.user.ApiTestExecutor;
+//import utils.JsonUtils;
+//
+//import java.util.List;
+//
+//public class EditProjectTest extends BaseTest {
+//
+//    @Test
+//    public void editProjectApiTest() {
+//
+//        EditProjectTestData testData =
+//                JsonUtils.readJson(
+//                        "testdata/project/editProject.json",
+//                        EditProjectTestData.class
+//                );
+//
+//        execute(testData, testData.getTestCases());
+//    }
+//
+//    private void execute(
+//            EditProjectTestData testData,
+//            List<EditProjectTestData.TestCase> cases
+//    ) {
+//
+//        for (EditProjectTestData.TestCase tc : cases) {
+//
+//            ApiTestExecutor.execute(
+//                    testData.getScenario(),
+//                    tc,
+//                    () -> ProjectApi.editProject(
+//                            tc.getRequest(),
+//                            tc.getRole()
+//                    )
+//            );
+//        }
+//    }
+//}
